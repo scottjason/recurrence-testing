bd3658ad771e59ca9f7d5912e4e36ad5941e18e1
Scott Jason <scottleejason@gmail.com>
2015-09-26 19:32:56 -0700

    logs all occurences for isWeekly for invtervals 1 through 10 and any combo of byDay

--- client/scripts/controllers/landing.js
+++ client/scripts/controllers/landing.js
@@ -11,26 +11,37 @@ function Landing($scope, $rootScope, $state, $timeout) {
 
   $scope.results = [];
 
-  $scope.setStartsOn = function(selectedDate) {
-    $scope.startsOn = selectedDate;
-    $scope.startsOnFormatted = moment(selectedDate).format('MMMM Do YYYY');
+  $scope.setStartsOn = function(dateSelected) {
+    $scope.startsOn = dateSelected;
+    $scope.startsOnFormatted = moment(dateSelected).format('MMMM Do YYYY');
   };
 
-  $scope.setStartsAt = function(selectedDate) {
-    $scope.startsAt = selectedDate;
-    $scope.startsAtFormatted = moment(selectedDate).format('h:mm a');
+  $scope.setStartsAt = function(dateSelected) {
+    $scope.startsAt = dateSelected;
+    $scope.startsAtFormatted = moment(dateSelected).format('h:mm a');
   };
 
-  $scope.setEndsAt = function(selectedDate) {
-    $scope.endsAt = selectedDate;
-    $scope.endsAtFormatted = moment(selectedDate).format('h:mm a');
+  $scope.setEndsAt = function(dateSelected) {
+    $scope.endsAt = dateSelected;
+    $scope.endsAtFormatted = moment(dateSelected).format('h:mm a');
+  };
+
+  $scope.setExpiresOn = function(dateSelected) {
+    $scope.activity.recurrence.expiresOn = dateSelected;
+    $scope.expiresOnFormatted = moment(dateSelected).endOf('Day').format('dddd, MMMM Do, YYYY h:mm a');
   };
 
   $scope.onSubmit = function() {
-    var isEndGreater = ($scope.endsAt.getTime() > $scope.startsAt.getTime());
-    var isValid = (isEndGreater && $scope.startsOn && $scope.startsAt && $scope.endsAt);
-    if (isValid) {
-      generateEventBounds();
+    if ($scope.startsOn && $scope.startsAt && $scope.endsAt && $scope.activity.recurrence.expiresOn) {
+      var isEndGreater = ($scope.endsAt.getTime() > $scope.startsAt.getTime());
+      var isExpGreater = ($scope.activity.recurrence.expiresOn.getTime() > $scope.endsAt.getTime());
+      console.log($scope.activity.recurrence.expiresOn)
+      console.log(isExpGreater);
+      if (isEndGreater && isExpGreater) {
+        generateEventBounds();
+      } else {
+        console.log('Invalid Input');
+      }
     } else {
       console.log('Invalid Input');
     }
@@ -48,15 +59,77 @@ function Landing($scope, $rootScope, $state, $timeout) {
     var duration = ($scope.endsAt.getTime() - $scope.startsAt.getTime());
     $scope.activity.dates.end = new Date(($scope.activity.dates.start).getTime() + duration);
 
-    if (!$scope.activity.interval) {
-      $scope.activity.interval = 1;
+    if (!$scope.activity.recurrence.interval) {
+      $scope.activity.recurrence.interval = 1;
     }
-    console.log($scope.activity)
-
+    generateWeekDays();
   }
 
+  function generateWeekDays() {
 
+    var byDay = [];
+    var byDayDisplay = [];
 
+    var isMonday = $('#monday').is(':checked');
+    var isTuesday = $('#tuesday').is(':checked');
+    var isWednesday = $('#wednesday').is(':checked');
+    var isThursday = $('#thursday').is(':checked');
+    var isFriday = $('#friday').is(':checked');
+    var isSaturday = $('#saturday').is(':checked');
+    var isSunday = $('#sunday').is(':checked');
+
+    if (isMonday) {
+      byDay.push(0);
+      byDayDisplay.push('Mon');
+    }
+    if (isTuesday) {
+      byDay.push(1);
+      byDayDisplay.push('Tue');
+    }
+    if (isWednesday) {
+      byDay.push(2);
+      byDayDisplay.push('Wed');
+    }
+    if (isThursday) {
+      byDay.push(3);
+      byDayDisplay.push('Thu');
+    }
+    if (isFriday) {
+      byDay.push(4);
+      byDayDisplay.push('Fri');
+    }
+    if (isSaturday) {
+      byDay.push(5);
+      byDayDisplay.push('Sat');
+    }
+    if (isSunday) {
+      byDay.push(6);
+      byDayDisplay.push('Sun');
+    }
+
+    $scope.activity.recurrence.byDay = byDay;
+
+    generateRRule();
+  }
+
+  function generateRRule() {
+
+    var rule = new RRule({
+      freq: RRule.WEEKLY,
+      interval: $scope.activity.recurrence.interval,
+      byweekday: $scope.activity.recurrence.byDay,
+      dtstart: $scope.activity.dates.start,
+      until: $scope.activity.recurrence.expiresOn
+    });
+
+    $scope.allOcurrences = rule.all();
+    $scope.numEvents = $scope.allOcurrences.length;
+    generateActivites();
+  }
+
+  function generateActivites() {
+
+  }
 
 
 
@@ -86,19 +159,6 @@ function Landing($scope, $rootScope, $state, $timeout) {
   });
 
 
-
-
-
-  $scope.onTimeSet = function(selectedDate) {
-    if (!$scope.activity.dates.start) {
-      $scope.activity.dates.start = selectedDate;
-      $scope.startsOnFormatted = moment(selectedDate).format('MMMM Do YYYY');
-      console.log($scope)
-    } else if (!$scope.activity.dates.end) {
-      $scope.activity.dates.end = selectedDate;
-    }
-    console.log(selectedDate);
-  };
 
 
 
